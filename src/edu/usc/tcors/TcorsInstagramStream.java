e02717814b6775d6f666590a54c341310ffcdbf7
karhai <karhai@karhaich.usc.edu>
2015-06-29 17:28:44 -0700

    Slowly updating Instagram live stream collector

--- src/edu/usc/tcors/TcorsInstagramStream.java
+++ src/edu/usc/tcors/TcorsInstagramStream.java
@@ -5,16 +5,28 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import org.jinstagram.exceptions.InstagramException;
-
-import twitter4j.conf.ConfigurationBuilder;
+import org.jinstagram.realtime.InstagramSubscription;
+import org.jinstagram.realtime.SubscriptionType;
 
 public class TcorsInstagramStream {
 	
 	public static void main(String[] args) throws InstagramException {
-		
+
 	}
 
-	private ConfigurationBuilder getConf() {
+	private void getStreamSubscription() {
+		Properties prop = getProps();
+		InstagramSubscription igSub = new InstagramSubscription()
+			.clientId(prop.getProperty("oauth.consumerKey"))
+			.clientSecret(prop.getProperty("oauth.consumerSecret"))
+			.object(SubscriptionType.TAGS)
+			.objectId("ecig")
+			.aspect("media")
+			.callback("http://karhaich.usc.edu/handleSubscription")
+			.verifyToken("ecigTagSubscription");
+	}
+	
+	private Properties getProps() {
 		Properties prop = new Properties();
 		try {
 			InputStream in = TcorsTwitterStream.class.getClassLoader().getResourceAsStream("jInstagram.properties");
@@ -22,13 +34,14 @@ public class TcorsInstagramStream {
 		} catch (IOException e) {
 			// log.error(e.toString());
 		}
-		ConfigurationBuilder twitterConf = new ConfigurationBuilder();
-		twitterConf.setIncludeEntitiesEnabled(true);
-		twitterConf.setDebugEnabled(Boolean.valueOf(prop.getProperty("debug")));
-		twitterConf.setOAuthAccessToken(prop.getProperty("oauth.accessToken"));
-		twitterConf.setJSONStoreEnabled(true);
+//		ConfigurationBuilder igConf = new ConfigurationBuilder();
+//		igConf.setIncludeEntitiesEnabled(true);
+//		igConf.setDebugEnabled(Boolean.valueOf(prop.getProperty("debug")));
+//		igConf.setOAuthConsumerKey(prop.getProperty("oauth.consumerKey"));
+//		igConf.setOAuthConsumerSecret(prop.getProperty("oauth.consumerSecret"));
+//		igConf.setJSONStoreEnabled(true);
 		
-		return twitterConf;
+		return prop;
 	}
 	
 }
