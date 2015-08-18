fe01752d3036a2c7fa37d469aef9025aa701fdd8
karhai <karhai@karhaich.usc.edu>
2015-08-17 17:29:30 -0700

    ... more Instagram updates...

--- src/edu/usc/tcors/TcorsInstagramScraper.java
+++ src/edu/usc/tcors/TcorsInstagramScraper.java
@@ -14,6 +14,7 @@ import org.jinstagram.Instagram;
 import org.jinstagram.auth.model.Token;
 import org.jinstagram.entity.comments.CommentData;
 import org.jinstagram.entity.common.Comments;
+import org.jinstagram.entity.common.Location;
 import org.jinstagram.entity.common.User;
 import org.jinstagram.entity.tags.TagMediaFeed;
 import org.jinstagram.entity.users.feed.MediaFeed;
@@ -71,42 +72,50 @@ public class TcorsInstagramScraper {
 		// for each search term
 		for (String term : test) {
 		
-			// #1 get data from Instagram
+			// get data from Instagram
+			
+			// use min_tag_id with highest already retrieved, to get newer ones
 			
 			TagMediaFeed mediaFeed = null;
 			List<MediaFeedData> mediaList = null;
+			int limit = 0;
 			
 			try {
-				mediaFeed = instagram.getRecentMediaTags(term, 100);
-				
+				// mediaFeed = instagram.getRecentMediaTags(term, 100); // max is only 33
+				mediaFeed = instagram.getRecentMediaTags(term, "1053911742112114767", "");
 				mediaList = mediaFeed.getData();
 				System.out.println("Step 1 size:" + mediaFeed.getData().size());
 				
 				MediaFeed recentMediaNextPage = instagram.getRecentMediaNextPage(mediaFeed.getPagination());
 				int counter = 1;
-				while (recentMediaNextPage.getPagination() != null && counter < 10) {
+				
+				limit = mediaFeed.getRemainingLimitStatus();
+				System.out.println("mediaFeed REMAIN:" + limit);
+				
+				while (recentMediaNextPage.getPagination() != null && limit > 4990) {
+					
 					mediaList.addAll(recentMediaNextPage.getData());
 					recentMediaNextPage = instagram.getRecentMediaNextPage(recentMediaNextPage.getPagination());
+					
 					counter++;
 					System.out.println("Counter (pages):" + counter);
+					limit = mediaFeed.getRemainingLimitStatus();
+					System.out.println("mediaFeed REMAIN:" + limit);
+					limit = 4800;
 				}
 				System.out.println("Size (x33):" + mediaList.size());
 			} catch (InstagramException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			System.out.println("mediaFeed APILS:" + mediaFeed.getAPILimitStatus());
-			System.out.println("mediaFeed REMAIN:" + mediaFeed.getRemainingLimitStatus());
-			
 			// #2 retrieve and store individual pieces of data
-			String instagram_sql = "REPLACE INTO instagram (id, createdTime, username, caption, likes, comments, url) " +
-					"VALUE (?, ?, ?, ?, ?, ?, ?)";
+			String instagram_sql = "REPLACE INTO instagram (id, createdTime, username, caption, likes, comments, url, location) " +
+					"VALUE (?, ?, ?, ?, ?, ?, ?, ?)";
 			
-			String comments_sql = "REPLACE INTO instagram_comments (id, parent_id, username, comment, created_time) " +
-					"VALUE (?, ?, ?)";
+			String comments_sql = "REPLACE INTO instagram_comments (id, parent_id, username, comment, createdTime) " +
+					"VALUE (?, ?, ?, ?, ?)";
 			
-			String users_sql = "REPLACE INTO instagram_users (id, bio, fullname, username) " +
+			String users_sql = "INSERT IGNORE INTO instagram_users (id, fullname, bio, username) " +
 					"VALUE (?, ?, ?, ?)";
 			
 			System.out.println("Updating DB...");
@@ -118,34 +127,38 @@ public class TcorsInstagramScraper {
 			
 				for (MediaFeedData mfd : mediaList) {
 					String caption = mfd.getCaption().getText();
-					// processComments(comments);
+					
 					Timestamp ts = new Timestamp(Long.parseLong(mfd.getCreatedTime())*1000);
 					String id = mfd.getId();
 					String url = mfd.getImages().getStandardResolution().getImageUrl();
 					int likes = mfd.getLikes().getCount();
-					// String location = mfd.getLocation().getName();
+					Location location = mfd.getLocation();
+					String location_name = "";
+					if (location != null) {
+						location_name = location.getName();
+					}
 					User user = mfd.getUser();
 					String username = user.getUserName();
-					String user_bio = user.getBio(); //string
-					String user_fullname = user.getFullName(); //string
-					String user_id = user.getId(); //string
-					// process user
-					// processUser(user);
+					String user_bio = user.getBio();
+					String user_fullname = user.getFullName();
+					String user_id = user.getId();
+
 					Comments comments = mfd.getComments();
 					int comments_count = comments.getCount();
+					
 					// process comments
 					List<CommentData> cd = comments.getComments();
 					for (CommentData comment_data : cd) {
 						String comment_id = comment_data.getId();
 						String comment_username = comment_data.getCommentFrom().getUsername();
 						String comment_text = comment_data.getText();
-						String comment_created_time = comment_data.getCreatedTime();
+						Timestamp comment_created_time = new Timestamp(Long.parseLong(comment_data.getCreatedTime())*1000);
 						
 						comments_ps.setString(1, comment_id);
 						comments_ps.setString(2, id);
 						comments_ps.setString(3, comment_username);
 						comments_ps.setString(4, comment_text);
-						comments_ps.setString(5, comment_created_time);
+						comments_ps.setTimestamp(5, comment_created_time);
 						
 						comments_ps.addBatch();
 					}
@@ -158,13 +171,15 @@ public class TcorsInstagramScraper {
 					instagram_ps.setInt(5, likes);
 					instagram_ps.setInt(6, comments_count);
 					instagram_ps.setString(7, url);
-					
-					users_ps.setString(1, user_id);
-					users_ps.setString(2, user_bio);
-					users_ps.setString(3, user_fullname);
-					users_ps.setString(4, username);
+					instagram_ps.setString(8, location_name);
 					
 					instagram_ps.addBatch();
+					
+					users_ps.setString(1, user_id);
+					users_ps.setString(2, user_fullname);
+					users_ps.setString(3, user_bio);
+					users_ps.setString(4, username);
+					
 					users_ps.addBatch();
 				}
 				
@@ -175,7 +190,6 @@ public class TcorsInstagramScraper {
 				System.out.println("Finished DB update");
 			
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
